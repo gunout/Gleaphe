1c4db9fd8395a2bdf763cecd9aa1736504124abd
gunout <88831335+gunout@users.noreply.github.com>
2023-08-21 14:28:12 +0400

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,5 @@
+var g = '162.210.192.98'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = '31010'; // PORT - Radionomy : 80
+var z = '/;stream.mp3'; //Shoutcast='/;stream.mp3'
+var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
+var l = ''; // Radionomy GUID (For Current Listen)
