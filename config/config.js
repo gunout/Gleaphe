b8d4d9350d8575030c36319c6a3155d931c5530a
gunout <88831335+gunout@users.noreply.github.com>
2024-11-03 20:16:58 +0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 var g = 'uk21freenew.listen2myradio.com'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
 var h = '16123'; // PORT - Radionomy : 80
-var z = '/;stream.mp3'; //Shoutcast='/;stream.mp3'
+var z = '/;stream.mp3'; // Shoutcast='/;stream.mp3'
 var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
 var l = ''; // Radionomy GUID (For Current Listen)
