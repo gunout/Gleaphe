38eb3e489aff1172958827bc7eb646ef3a98a4bf
Julien <88831335+gunout@users.noreply.github.com>
2025-01-28 15:32:03 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-var g = '82.145.41.8'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
-var h = '16123'; // PORT - Radionomy : 80
+var g = 82.145.41.8; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = 16123 ; // PORT - Radionomy : 80
 var z = '/;stream.mp3'; // Shoutcast='/;stream.mp3'
 var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
 var l = ''; // Radionomy GUID (For Current Listen)
