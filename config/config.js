85cc5b2dfdec406de79d20893a66615a6805631f
Julien Talabaza <88831335+gunout@users.noreply.github.com>
2025-02-23 13:35:56 +0400

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,5 @@
+var g = 'URL'; // URL SANS HTTP:// - IP WITHOUT HTTP:// - Radionomy : streaming.radionomy.com
+var h = 'PORT'; // PORT - Radionomy : 80
+var z = ''; //Shoutcast='/;stream.mp3'
+var i = ''; // Icecast='/mount' - Shoutcast='' - Radionomy : /radioname
+var l = ''; // Radionomy GUID (For Current Listen)
