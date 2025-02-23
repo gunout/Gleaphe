ed794d7b323dd6cf14d884d80d0e48230c35cbb2
Julien Talabaza <88831335+gunout@users.noreply.github.com>
2025-02-23 13:35:38 +0400

    Delete config directory

--- config/config.js
+++
@@ -1,4 +0,0 @@
-var g = '102.35.99.37'; // Remplacez par l'URL ou l'IP de votre serveur
-var h = '8002'; // Remplacez par le port de votre serveur
-var z = '/;stream.mp3'; // Chemin du flux Shoutcast
-var i = ''; // Laissez vide pour Shoutcast
